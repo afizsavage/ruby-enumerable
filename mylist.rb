a17c85ea46931cb3b37b6a1cbeefeeb5d5e63190
Afiz Savage <savageafiz9@gmail.com>
2022-03-12 16:08:29 +0000

    Created mylist class

--- mylist.rb
+++ mylist.rb
@@ -1,3 +1,23 @@
-class MyList
+require_relative './myenumerable'
 
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    (0...@list.length).each do |i|
+      yield @list[i]
+    end
+  end
 end
+
+list = MyList.new(2,4,6,8)
+
+puts(list.all_items { |e| e < 5 })
+puts(list.all_items { |e| e > 5 })
+puts(list.any_items { |e| e == 2 })
+puts(list.any_items { |e| e == 5 })
+puts(list.filter_items(&:even?))
